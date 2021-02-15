f8bd3033cb83e83623c520b69d94272d5a3d199c
sbulen <bulens@pacbell.net>
2021-02-15 14:58:20 -0800

    Label test

--- Themes/default/languages/ShawnTest.english.php
+++ Themes/default/languages/ShawnTest.english.php
@@ -8,10 +8,14 @@ $txt['st 2'] = 'Shawn test 2';
 $txt['st 3'] = 'Shawn test 3 modified';
 $txt['st 4'] = 'Shawn test 4 modified';
 $txt['st 5'] = 'Shawn test 5';
-$txt['st 6'] = 'Shawn test 6 SOME MORE CHANGES';
+$txt['st 6'] = 'Shawn test 6 EVEN MORE CHANGES';
 $txt['st 7'] = 'Shawn test 7 SOME MORE CHANGES';
 $txt['st 8'] = 'Shawn test 8';
 $txt['st 9'] = 'Shawn test 9';
 $txt['st 10'] = 'Shawn test 10 SOME MORE CHANGES';
+$txt['st 11'] = 'Shawn test 11 EVEN MORE CHANGES';
+$txt['st 12'] = 'Shawn test 12 EVEN MORE CHANGES';
+$txt['st 13'] = 'Shawn test 13 EVEN MORE CHANGES';
+$txt['st 14'] = 'Shawn test 14 EVEN MORE CHANGES';
 
 ?>
