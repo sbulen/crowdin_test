6d7ad4e611cf197b0c5d66beec83c93fcf3d637c
sbulen <bulens@pacbell.net>
2021-02-15 11:54:38 -0800

    One more data test for changes

--- Themes/default/languages/ShawnTest.english.php
+++ Themes/default/languages/ShawnTest.english.php
@@ -3,15 +3,15 @@
 
 global $settings, $scripturl;
 
-$txt['st 1'] = 'Shawn test 1';
+$txt['st 1'] = 'Shawn test 1 SOME MORE CHANGES';
 $txt['st 2'] = 'Shawn test 2';
 $txt['st 3'] = 'Shawn test 3 modified';
 $txt['st 4'] = 'Shawn test 4 modified';
 $txt['st 5'] = 'Shawn test 5';
-$txt['st 6'] = 'Shawn test 6';
-$txt['st 7'] = 'Shawn test 7';
+$txt['st 6'] = 'Shawn test 6 SOME MORE CHANGES';
+$txt['st 7'] = 'Shawn test 7 SOME MORE CHANGES';
 $txt['st 8'] = 'Shawn test 8';
 $txt['st 9'] = 'Shawn test 9';
-$txt['st 10'] = 'Shawn test 10';
+$txt['st 10'] = 'Shawn test 10 SOME MORE CHANGES';
 
 ?>
